b1dd2c6200d4c3d0333432ac0c2dfeab2989deb7
Heandy27 <andy_heredia2008@hotmail.com>
2025-08-18 17:47:53 -0500

    feat: set up Core Data model and define entity relationships
    Started integrating Core Data by designing the initial data model.
    Defined the entities and their relationships to ensure a solid
    foundation for data persistence and future app features.

--- UltimatePortfolio_AH/UltimatePortfolio_AHApp.swift
+++ UltimatePortfolio_AH/UltimatePortfolio_AHApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct UltimatePortfolio_AHApp: App {
+    
+    @StateObject var dataController = DataController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
         }
     }
 }
