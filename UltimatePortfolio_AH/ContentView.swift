23f812ff1d20975dcc60accdb01a9cff772a71fd
Heandy27 <andy_heredia2008@hotmail.com>
2025-08-18 14:59:06 -0500

    My first commit

---
+++ UltimatePortfolio_AH/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UltimatePortfolio_AH
+//
+//  Created by Andy Heredia on 18/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UltimatePortfolio_AH/UltimatePortfolio_AHApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolio_AHApp.swift
+//  UltimatePortfolio_AH
+//
+//  Created by Andy Heredia on 18/8/25.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolio_AHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
