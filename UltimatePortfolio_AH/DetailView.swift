be59569fe70169ec3c031e3b62edd3cf7099bcab
Heandy27 <andy_heredia2008@hotmail.com>
2025-08-19 15:54:21 -0500

    feat: implement basic UI to validate Core Data model
    Configured the initial user interface to interact with the Core Data model.
    This allows verifying that entities and relationships are correctly set up
    and ensures data persistence works as expected during testing.

---
+++ UltimatePortfolio_AH/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  UltimatePortfolio_AH
+//
+//  Created by Andy Heredia on 19/8/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Detail")
+    }
+}
+
+#Preview {
+    DetailView()
+}
--- UltimatePortfolio_AH/UltimatePortfolio_AHApp.swift
+++ UltimatePortfolio_AH/UltimatePortfolio_AHApp.swift
@@ -14,7 +14,13 @@ struct UltimatePortfolio_AHApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationSplitView {
+                SidebarView()
+            } content: {
+                ContentView()
+            } detail: {
+                DetailView()
+            }
                 .environment(\.managedObjectContext, dataController.container.viewContext)
                 .environmentObject(dataController)
         }
