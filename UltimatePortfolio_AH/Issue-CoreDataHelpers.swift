855b6e7ac8863315a9369886cd169c62fb36927e
Heandy27 <andy_heredia2008@hotmail.com>
2025-08-19 16:49:34 -0500

    refactor: improve Core Data optional handling
    Adjusted Core Data entities to better align with Swift’s optional semantics.
    Implemented clearer strategies for managing nil values, reducing boilerplate
    and improving code readability when working with managed objects.

---
+++ UltimatePortfolio_AH/Issue-CoreDataHelpers.swift
@@ -0,0 +1,61 @@
+//
+//  Issue-CoreDataHelpers.swift
+//  UltimatePortfolio_AH
+//
+//  Created by Andy Heredia on 19/8/25.
+//
+
+import Foundation
+
+extension Issue {
+    
+    var issueTitle: String {
+        get { title ?? "" }
+        set { title = newValue }
+    }
+    
+    var issueContent: String {
+        get { content ?? "" }
+        set { content = newValue }
+    }
+    
+    var issueCreationDate: Date {
+        creationDate ?? .now
+    }
+    
+    var issueModificationDate: Date {
+        modificationDate ?? .now
+    }
+    
+    // NSSet Creo quie aqui añadimos la relacion que tiene issues con tags
+    var issueTags: [Tag] {
+        let result = tags?.allObjects as? [Tag] ?? []
+        return result.sorted()
+    }
+    
+    static var example: Issue {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let issue = Issue(context: viewContext)
+        issue.title = "Example Issue"
+        issue.content = "This is an example issue."
+        issue.priority = 2
+        issue.creationDate = .now
+        return issue
+    }
+    
+}
+
+extension Issue: Comparable {
+    public static func <(lhs: Issue, rhs: Issue) -> Bool {
+        let left = lhs.issueTitle.localizedLowercase
+        let right = rhs.issueTitle.localizedLowercase
+
+        if left == right {
+            return lhs.issueCreationDate < rhs.issueCreationDate
+        } else {
+            return left < right
+        }
+    }
+}
