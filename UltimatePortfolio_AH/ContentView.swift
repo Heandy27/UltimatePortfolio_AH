15d1e90f8409d3acc7cbbfb6de1deb281ba48992
Heandy27 <andy_heredia2008@hotmail.com>
2025-08-22 12:01:31 -0500

    feat: implement advanced issue filtering with Core Data search
    Added initial support for filtering issues by search text,
    leveraging Core Data predicates for titles and content. This
    lays the groundwork for more powerful filtering capabilities
    beyond the basic implementation, ensuring scalability and
    better user experience when managing issues.

--- UltimatePortfolio_AH/ContentView.swift
+++ UltimatePortfolio_AH/ContentView.swift
@@ -11,43 +11,22 @@ struct ContentView: View {
     
     @EnvironmentObject var dataController: DataController
     
-    
-    // Propiedad calculada: devuelve issues filtrados de Core Data
-    var issues: [Issue] {
-       
-        let filter = dataController.selectedFilter ?? .all // Filtro actual o "all"
-        // Se declara un array temporal para guardar los issues que se van a obtener.
-        var allIssues: [Issue]
-        
-        if let tag = filter.tag {
-            // tag.issues?.allObjects convierte el conjunto (NSSet) de issues en un array.
-            allIssues = tag.issues?.allObjects as? [Issue] ?? [] // Issues por tag
-        } else {
-            /*
-             Si el filtro no tiene un tag
-             Se crea una fetchRequest() para pedir todos los issues en Core Data.
-             try? ... hace el fetch, y si falla devuelve [].
-             */
-            let request = Issue.fetchRequest() // Todos los issues
-            // Solo tráeme los registros cuya modificationDate sea posterior a la fecha mínima definida en el filtro.
-            request.predicate = NSPredicate(format: "modificationDate > %@", filter.minModificationDate as NSDate)
-            allIssues = (try? dataController.container.viewContext.fetch(request)) ?? []
-        }
-        
-        // Siempre los devuelve ordenados con .sorted().
-        return allIssues.sorted()
-    }
-    
     var body: some View {
         List(selection: $dataController.selectedIssue) {
-            ForEach(issues) { issue in
+            ForEach(dataController.issuesForSelectedFilter()) { issue in
                 IssueRowView(issue: issue)
             }
             .onDelete(perform: delete)
         }
         .navigationTitle("Issues")
+        .searchable(text: $dataController.filterText, tokens: $dataController.filterTokens, suggestedTokens: .constant(dataController.suggestedFilterTokens), prompt: "Filter issues, or type # to add tags") { tag in
+            Text(tag.tagName)
+        }
     }
     func delete(_ offsets: IndexSet) {
+        
+        let issues = dataController.issuesForSelectedFilter()
+        
         for offset in offsets { // índices a borrar (de onDelete)
             let item = issues[offset] // obtiene el Tag en ese índice
             dataController.delete(item) // elimina en Core Data y guarda
